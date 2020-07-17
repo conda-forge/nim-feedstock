6e6df229479411df3b0f3e2402def3617d95b816
Marcel Bargull <marcel.bargull@udo.edu>
2020-07-17 23:13:12 +0200

    fix hard-coded compiler names

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,11 @@ set -vexu -o pipefail
 
 pushd nim
 # inject compilers
-echo "gcc.exe = \"x86_64-conda_cos6-linux-gnu-gcc\"" >> config/nim.cfg
-echo "gcc.cpp.exe = \"x86_64-conda_cos6-linux-gnu-gcc\"" >> config/nim.cfg
-echo "gcc.linkerexe = \"x86_64-conda_cos6-linux-gnu-gcc\"" >> config/nim.cfg
-echo "clang.exe = \"x86_64-conda_cos6-linux-gnu-gcc\"" >> config/nim.cfg
-echo "clang.linkerexe = \"x86_64-conda_cos6-linux-gnu-gcc\"" >> config/nim.cfg
+echo "gcc.exe = \"$(basename "${CC}")\"" >> config/nim.cfg
+echo "gcc.cpp.exe = \"$(basename "${CC}")\"" >> config/nim.cfg
+echo "gcc.linkerexe = \"$(basename "${CC}")\"" >> config/nim.cfg
+echo "clang.exe = \"$(basename "${CC}")\"" >> config/nim.cfg
+echo "clang.linkerexe = \"$(basename "${CC}")\"" >> config/nim.cfg
 ./build.sh
 bin/nim c koch
 ./koch tools
